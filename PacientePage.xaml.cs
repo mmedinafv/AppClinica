7fc0f24cc42f5ae4b33a066e3cb13f552ce7bcce
Marissa Medina <134749304+mmedinafv@users.noreply.github.com>
2025-07-26 11:14:39 -0600

    Add files via upload

---
+++ PacientePage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1.Pages;
+
+
+public partial class PacientePage : ContentPage
+{
+    public PacientePage()
+    {
+        InitializeComponent();
+        BindingContext = new SignosVitalesViewModel();
+    }
+}
